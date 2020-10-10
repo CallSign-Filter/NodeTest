0e3941d882bab0a17dc69b48311cca00af0f4be7
Brandon Hessler <coding@brandonhessler.com>
2020-10-10 16:27:05 -0700

    Tesing out submit function

---
+++ insert.php
@@ -0,0 +1,35 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$servername = "localhost";
+$database = "u379769495_bbq";
+$username = "u379769495_root";
+$password = "USMC44eva";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Escape user inputs for security
+$bbqDayId = $_REQUEST['bbqDayId'];
+$hostName = mysqli_real_escape_string($conn, $_REQUEST['hostName']);
+$hostTeam = mysqli_real_escape_string($conn, $_REQUEST['hostTeam']);
+
+// Attempt insert query execution
+$sql = "UPDATE bbqDay SET hostName= $hostName, hostTeam = $hostTeam WHERE bbqDayId= $bbqDayId";
+if(mysqli_query($conn, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+
+// Close connection
+mysqli_close($conn);
+?>
--- test.php
+++ test.php
@@ -15,71 +15,6 @@ echo "Connected successfully";
 
 <html>
  <head>
- <script>
- var modal = document.getElementById("myModal");
-
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
-
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
-
- // When the user clicks on the button, open the modal
- btn.onclick = function() {
-   modal.style.display = "block";
- }
-
- // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
-   modal.style.display = "none";
- }
-
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-   if (event.target == modal) {
-     modal.style.display = "none";
-   }
- }
- </script>
- <style>
- /* The Modal (background) */
- .modal {
-   display: none; /* Hidden by default */
-   position: fixed; /* Stay in place */
-   z-index: 1; /* Sit on top */
-   left: 0;
-   top: 0;
-   width: 100%; /* Full width */
-   height: 100%; /* Full height */
-   overflow: auto; /* Enable scroll if needed */
-   background-color: rgb(0,0,0); /* Fallback color */
-   background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
- }
-
- /* Modal Content/Box */
- .modal-content {
-   background-color: #fefefe;
-   margin: 15% auto; /* 15% from the top and centered */
-   padding: 20px;
-   border: 1px solid #888;
-   width: 80%; /* Could be more or less, depending on screen size */
- }
-
- /* The Close Button */
- .close {
-   color: #aaa;
-   float: right;
-   font-size: 28px;
-   font-weight: bold;
- }
-
- .close:hover,
- .close:focus {
-   color: black;
-   text-decoration: none;
-   cursor: pointer;
- }
- </style>
  </head>
  <body>
  <h1>PHP connect to MySQL</h1>
@@ -98,24 +33,18 @@ while ($row = mysqli_fetch_array($result)) {
 mysqli_close($conn);
 ?>
 <button type="button" onclick="prompt('What is your name?')">Sign up</button>
-<button id="myBtn">Open Modal</button>
-<div id="myModal" class="modal">
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some other text in the Modal..</p>
-  </div>
-
-</div>
+<form action="insert.php" method="post" onsubmit="setTimeout(function () { window.location.reload(); }, 30)">
+    <input type="hidden" id="bbqDayId" name="bbqDayId" value="5"/>
+    <p>
+        <label for="hostName">Host Name:</label>
+        <input type="text" name="hostName" id="hostName">
+    </p>
+    <p>
+        <label for="hostTeam">Host Team:</label>
+        <input type="text" name="hostTeam" id="hostTeam">
+    </p>
+    <input type="submit" value="Submit">
+</form>
 </body>
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
 </html>
