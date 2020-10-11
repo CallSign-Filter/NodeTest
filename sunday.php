2cdf2b6546e30c04626234ca01b4291f495b2df6
Brandon Hessler <coding@brandonhessler.com>
2020-10-10 17:58:43 -0700

    sad face

--- sunday.php
+++ sunday.php
@@ -145,7 +145,9 @@ $row = mysqli_fetch_array($result);
                     <div class="calendar" style="padding-top: 5px">
                         <a class="center"><?php echo $row['fantasyWeek']?>: <?php echo substr($row['bbqDate'], 5)?></a>
                     </div>
-                    <?php echo ("a" == "b") ? <a>1</a> : <a>2</a> ?>
+                    <?php if (1 < 2) { ?>
+                    <a>wtf</a>
+                    <?php } ?>
                 </div>
             <?php } mysqli_close($conn); ?>
             </article>
