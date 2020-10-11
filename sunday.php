bf0216545c6ea305d87e7d95b662bf41d49bcb8f
Brandon Hessler <coding@brandonhessler.com>
2020-10-10 17:19:23 -0700

    format

--- sunday.php
+++ sunday.php
@@ -145,10 +145,10 @@ $row = mysqli_fetch_array($result);
                     <div class="calendar" style="padding-top: 5px">
                         <a class="center"><?php echo $row['fantasyWeek']?>: <?php echo substr($row['bbqDate'], 5)?></a>
                     </div>
-                    <?php echo if($row['hostName'] == '') {
-                                    'sign up'
+                    <?php if($row['hostName'] = '') {
+                                    echo 'sign up'
                                     } else {
-                                     ' ' . $row['hostTeam']
+                                     echo ' ' . $row['hostTeam']
                                       }?>
                 </div>
             <?php } mysqli_close($conn); ?>
